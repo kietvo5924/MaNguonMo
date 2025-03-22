5d3d1eab17cd4254d6407d86ca40db5d2da05c12
kietvo5924 <kietcun100@gmail.com>
2025-03-22 10:44:26 +0700

    update orders

---
+++ src/main/java/com/example/backend/DTO/OrderDTO.java
@@ -0,0 +1,58 @@
+package com.example.backend.DTO;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class OrderDTO {
+    private Long id;
+    private Long userId;
+    private LocalDateTime orderDate;
+    private String status;
+    private Double totalPrice;
+    private String shippingAddress;
+    private List<OrderDetailDTO> orderDetails;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public LocalDateTime getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(LocalDateTime orderDate) {
+		this.orderDate = orderDate;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public Double getTotalPrice() {
+		return totalPrice;
+	}
+	public void setTotalPrice(Double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+	public String getShippingAddress() {
+		return shippingAddress;
+	}
+	public void setShippingAddress(String shippingAddress) {
+		this.shippingAddress = shippingAddress;
+	}
+	public List<OrderDetailDTO> getOrderDetails() {
+		return orderDetails;
+	}
+	public void setOrderDetails(List<OrderDetailDTO> orderDetails) {
+		this.orderDetails = orderDetails;
+	}
+    
+    
+}
---
+++ src/main/java/com/example/backend/DTO/OrderDetailDTO.java
@@ -0,0 +1,55 @@
+package com.example.backend.DTO;
+
+public class OrderDetailDTO {
+    private Long productId;
+    private String productName;
+    private Integer quantity;
+    private Double price;
+    private Double totalAmount;
+    private String versionName;
+    private String colorName;
+	public Long getProductId() {
+		return productId;
+	}
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public Double getTotalAmount() {
+		return totalAmount;
+	}
+	public void setTotalAmount(Double totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+	public String getVersionName() {
+		return versionName;
+	}
+	public void setVersionName(String versionName) {
+		this.versionName = versionName;
+	}
+	public String getColorName() {
+		return colorName;
+	}
+	public void setColorName(String colorName) {
+		this.colorName = colorName;
+	}
+    
+    
+}
